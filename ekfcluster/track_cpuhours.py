1e273718986493d37cb9f5220f97979429fb6958
Erin Kado-Fong <erin.kado-fong@yale.edu>
2022-10-31 14:58:01 -0400

    suppress double-counting

--- ekfcluster/track_cpuhours.py
+++ ekfcluster/track_cpuhours.py
@@ -11,17 +11,28 @@ args = parser.parse_args ()
 date = args.date
 
 # \\ get sacct output
-cmd = f"sacct -S{date} -oalloccpu,cputime"
+cmd = f"sacct -S{date} -ojobid,alloccpu,cputime"
 p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
 output = p.communicate()[0]
 
 # \\ compute time used
-timing = output.decode('utf-8').split('\n')[2:]
+timing = output.decode('utf-8').split('\n')[2:-1]
 total_time = 0.
-for row in timing:
+for row in timing:    
     vals = row.split()
-    ncpu = int(vals[0])
-    nhr,nmin,nsec = [ int(x) for x in vals[1].split(':') ]
+    # \\ do not double (triple) count due to .batch and .extern
+    # \\ jobIDs
+    if '.' in vals[0]:
+        continue
+        
+    ncpu = int(vals[1])
+    nhr,nmin,nsec = [ int(x) for x in vals[2].split(':') ]
     ntime = nhr + nmin/60. + nsec/3600.
     total_time += ncpu * ntime
-print(total_time)
+
+if total_time < (1./60.):
+    print ( '%.0f seconds of CPU time used since %s' % (total_time * 3600., date) )
+elif total-time < 1.:
+    print ( '%.0f minutes of CPU time used since %s' % (total_time * 60., date) )
+else:
+    print ( '%.0f hours of CPU time used since %s' % (total_time, date) )
