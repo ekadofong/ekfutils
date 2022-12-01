02bbef6b2dfb3677496a3c32e033eae6e7517f8f
Erin Kado-Fong <erin.kado-fong@yale.edu>
2022-12-01 11:02:21 -0500

    add kwargs

--- ekfplot/ekfplot/plot.py
+++ ekfplot/ekfplot/plot.py
@@ -1,13 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def errorbar ( x, y, ylow, yhigh, ax=None ):
+def errorbar ( x, y, ylow, yhigh, ax=None, **kwargs ):
     if ax is None:
         ax = plt.subplot(111)
     
     yerr =  np.array([[yhigh - y], [y-ylow]]).reshape(2,-1)
     ax.errorbar ( x, 
                   y, 
-                  yerr = yerr 
+                  yerr = yerr,
+                  **kwargs
                 )
     return ax
