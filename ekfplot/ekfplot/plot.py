895c1def7ec8e2ff2bd0a048e89019b959d5b6a8
Erin Kado-Fong <erin.kado-fong@yale.edu>
2023-01-19 15:38:57 -0500

    add ability to overlay colormapped scatter

--- ekfplot/ekfplot/plot.py
+++ ekfplot/ekfplot/plot.py
@@ -1,7 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def errorbar ( x, y, xlow=None, xhigh=None, ylow=None, yhigh=None, ax=None, **kwargs ):
+def errorbar ( x, y, xlow=None, xhigh=None, ylow=None, yhigh=None, ax=None, c=None,
+               scatter_kwargs={}, **kwargs ):
     if ax is None:
         ax = plt.subplot(111)
         
@@ -20,10 +21,22 @@ def errorbar ( x, y, xlow=None, xhigh=None, ylow=None, yhigh=None, ax=None, **kw
     xerr =  np.array([[xhigh - x], [x-xlow]]).reshape(2,-1)    
     yerr =  np.array([[yhigh - y], [y-ylow]]).reshape(2,-1)
     
-    ax.errorbar ( x, 
-                  y, 
-                  xerr = xerr,
-                  yerr = yerr,
-                  **kwargs
-                )
+    if c is None:
+        ax.errorbar ( x, 
+                    y, 
+                    xerr = xerr,
+                    yerr = yerr,
+                    **kwargs
+                    )
+    else:
+        kwargs['markersize'] = 0
+        ax.errorbar ( x, 
+                    y, 
+                    xerr = xerr,
+                    yerr = yerr,
+                    zorder=9,
+                    **kwargs
+                    )
+        im = ax.scatter ( x, y, c=c, zorder=10, **scatter_kwargs )
+        return ax, im
     return ax
