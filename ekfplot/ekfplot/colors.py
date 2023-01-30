f01600e76efd7d625aa6dc12465de09839b616fd
Erin Kado-Fong <erin.kado-fong@yale.edu>
2023-01-30 10:37:21 -0500

    add more general modulate fn

--- ekfplot/ekfplot/colors.py
+++ ekfplot/ekfplot/colors.py
@@ -19,10 +19,20 @@ class ColorBase ( object ):
             else:
                 self.base = color
         elif system == 'hsv':
-            self.base = colorsys.hsv_to_rgb ( color )
+            self.base = colorsys.hsv_to_rgb (*color )
         else:
             raise KeyError (f'Color system {system} not understood!')
 
+    def modulate ( self, dl, ds ):
+        hls_color = colorsys.rgb_to_hls ( *self.base )
+        modulated = [
+            hls_color[0], 
+            max(0.,min(1.,hls_color[1]+dl)), 
+            max(0.,min(1.,hls_color[2]+ds)) 
+            ]
+        modulated = colorsys.hls_to_rgb ( *modulated )
+        return modulated
+    
     def lighten ( self, value ):
         hls_color = colorsys.rgb_to_hls ( *self.base )
         lightened = [hls_color[0], max(0.,min(1.,hls_color[1]+value)), hls_color[2] ]
@@ -34,6 +44,15 @@ class ColorBase ( object ):
     def complement ( self ):
         complementary = 1. - np.array(self.base)
         return tuple(complementary)
+    
+    def evenly_spaced_colors (self, ncolors):
+        hsv = colorsys.rgb_to_hsv(*self.base)
+        
+        wedge = 1./ncolors
+        hues = [ (hsv[0] + wedge*idx) % 1. for idx in range(ncolors)] 
+        hsv_l = [ ColorBase( (h, hsv[1], hsv[2]), 'hsv') for h in hues ]
+        return hsv_l
+        
         
     def sequential_cmap ( self, end_color='w', end_color_system='mpl_named', reverse=False):
         end_color = ColorBase ( end_color, end_color_system )
