8aaf51acbfc6f76e3840ecfe63f03ff13913a9d5
Erin Kado-Fong <erin.kado-fong@yale.edu>
2023-03-06 11:31:02 -0500

    add alpha

--- ekfplot/ekfplot/colors.py
+++ ekfplot/ekfplot/colors.py
@@ -16,21 +16,30 @@ class ColorBase ( object ):
     '''
     def __init__ ( self, color, system='hexcolor' ):
         if system == 'hexcolor':
-            self.base = mpc.to_rgb ( color )
+            self.base =  np.array(mpc.to_rgba ( color ))
         elif system=='mpl_named':
-            self.base = mpc.to_rgb (color)
+            self.base = np.array(mpc.to_rgba (color))
         elif system == 'rgb':
             if max(color) > 1.:
-                self.base = np.array(color)/255.
+                self.base = np.concatenate([np.array(color)/255.,[1.]])
             else:
-                self.base = color
+                self.base = np.concatenate([color,[1.]])
         elif system == 'hsv':
-            self.base = colorsys.hsv_to_rgb (*color )
+            self.base = np.concatenate([colorsys.hsv_to_rgb (*color ), [1.]])
+        elif system == 'rgba':
+            if max(color) > 1.:
+                self.base = np.concatenate([np.array(color[:3])/255.,color[-1:]])
+            else:
+                self.base = color        
         else:
             raise KeyError (f'Color system {system} not understood!')
 
+    @property 
+    def rgb_base ( self ):
+        return self.base[:-1]
+
     def modulate ( self, dl=0., ds=0. ):
-        hls_color = colorsys.rgb_to_hls ( *self.base )
+        hls_color = colorsys.rgb_to_hls ( *self.rgb_base )
         modulated = [
             hls_color[0], 
             max(0.,min(1.,hls_color[1]+dl)), 
@@ -41,8 +50,14 @@ class ColorBase ( object ):
         modulated = ColorBase ( modulated, system='rgb' )
         return modulated
     
+    def clarify ( self, alpha_new ):
+        updated_base = self.base.copy()
+        updated_base[-1] = alpha_new
+        new_cb = ColorBase ( updated_base, system='rgba' )
+        return new_cb
+    
     def lighten ( self, value ):
-        hls_color = colorsys.rgb_to_hls ( *self.base )
+        hls_color = colorsys.rgb_to_hls ( *self.rgb_base )
         lightened = [hls_color[0], max(0.,min(1.,hls_color[1]+value)), hls_color[2] ]
 
         output_rgb = colorsys.hls_to_rgb ( *lightened )
@@ -50,11 +65,11 @@ class ColorBase ( object ):
         
     @property
     def complement ( self ):
-        complementary = 1. - np.array(self.base)
+        complementary = 1. - np.array(self.rgb_base)
         return ColorBase(tuple(complementary), system='rgb')
     
     def evenly_spaced_colors (self, ncolors):
-        hsv = colorsys.rgb_to_hsv(*self.base)
+        hsv = colorsys.rgb_to_hsv(*self.rgb_base)
         
         wedge = 1./ncolors
         hues = [ (hsv[0] + wedge*idx) % 1. for idx in range(ncolors)] 
@@ -62,8 +77,13 @@ class ColorBase ( object ):
         return hsv_l
         
         
-    def sequential_cmap ( self, end_color='w', end_color_system='mpl_named', reverse=False):
+    def sequential_cmap ( self, end_color='w', end_color_system='mpl_named', reverse=False, fade=1. ):
         end_color = ColorBase ( end_color, end_color_system )
+        if fade<1.:
+            end_color = end_color.clarify ( fade )
+            
+        if fade:
+            end_color_code = end_color
         if reverse:
             cmap = mpc.LinearSegmentedColormap.from_list ( 'sequential', [end_color.base,self.base], )
         else:
