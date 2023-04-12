333b3bf01a5469667ee7293a36da1133894ef48f
Erin Kado-Fong <erin.kado-fong@yale.edu>
2023-04-12 09:28:39 -0400

    add latex to float parsing

--- ekfparse/ekfparse/strings.py
+++ ekfparse/ekfparse/strings.py
@@ -1,3 +1,4 @@
+import re
 import numpy as np
 
 def where_substring (arr, substring):
@@ -5,3 +6,15 @@ def where_substring (arr, substring):
     Return indices of array [arr] that contain the substring [substring]
     '''
     return np.flatnonzero(np.core.defchararray.find ( arr, substring )!=-1)
+
+def latex_to_float ( x ):
+    '''
+    Converts LaTeX of the form
+    $ A \times 10^{B} $ 
+    to 
+    float(A * 10.**B)
+    '''
+    first = float(re.findall( '(?<=\$).*(?=\\\\)', x )[0])
+    exponent = float(re.findall( '(?<=10\^\{).*(?=\})', x )[0])
+    result = first * 10**exponent
+    return result
