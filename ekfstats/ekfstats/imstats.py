ddcf4ede6e800558bdce5c14ec03f3338445202e
Erin Kado-Fong <erin.kado-fong@yale.edu>
2023-10-30 12:12:15 -0400

    halfhearted gini computation

---
+++ ekfstats/ekfstats/imstats.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def gini(x, w=None):
+    sorted_x = np.sort(x)
+    n = len(x)
+    cumx = np.cumsum(sorted_x, dtype=float)
+    # The above formula, with all weights equal to 1 simplifies to:
+    return (n + 1 - 2 * np.sum(cumx) / cumx[-1]) / n
