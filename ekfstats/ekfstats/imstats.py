e5b3fa52d663aba7e710026f7e4150067c3bec24
Erin Kado-Fong <erin.kado-fong@yale.edu>
2023-10-31 10:48:20 -0400

    allow for ND arrays and cuts on pct

--- ekfstats/ekfstats/imstats.py
+++ ekfstats/ekfstats/imstats.py
@@ -1,8 +1,15 @@
 import numpy as np
 
-def gini(x, w=None):
+def gini(x, w=None, qt_cut=0.):
+    x = x.flatten()
+    x = x[x>np.nanquantile(x, qt_cut)]
     sorted_x = np.sort(x)
     n = len(x)
-    cumx = np.cumsum(sorted_x, dtype=float)
+    indices = np.arange(1,1+n)
+    interior = np.sum( (2.*indices - n - 1.)*abs(sorted_x) ) 
+    factor = (np.mean(abs(x)) * n * (n-1.) )**-1
+    return interior * factor
+    #cumx = np.cumsum(sorted_x, dtype=float)
     # The above formula, with all weights equal to 1 simplifies to:
-    return (n + 1 - 2 * np.sum(cumx) / cumx[-1]) / n
+    #return (n + 1 - 2 * np.sum(cumx) / cumx[-1]) / n
+    
