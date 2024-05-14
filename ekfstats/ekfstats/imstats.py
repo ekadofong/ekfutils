4a2d81f41b56dd06aca16987e4806ba408e23ed1
Erin Kado-Fong <erin.kado-fong@yale.edu>
2024-05-14 10:07:48 -0400

    centering convenience

--- ekfstats/ekfstats/imstats.py
+++ ekfstats/ekfstats/imstats.py
@@ -12,4 +12,29 @@ def gini(x, w=None, qt_cut=0.):
     #cumx = np.cumsum(sorted_x, dtype=float)
     # The above formula, with all weights equal to 1 simplifies to:
     #return (n + 1 - 2 * np.sum(cumx) / cumx[-1]) / n
-    
+
+def get_center(arr):
+    """
+    Calculate the coordinates of the center of the array.
+
+    Parameters:
+    - arr (numpy.ndarray): The input array.
+
+    Returns:
+    - numpy.ndarray: The coordinates of the center of the array.
+    """
+    ccoord = np.array(arr.shape) // 2
+    return ccoord
+
+def get_centerval(arr):
+    """
+    Get the value at the center of the array.
+
+    Parameters:
+    - arr (numpy.ndarray): The input array.
+
+    Returns:
+    - int or float: The value at the center of the array.
+    """
+    ccoord = get_center(arr)
+    return arr[ccoord[0], ccoord[1]]
