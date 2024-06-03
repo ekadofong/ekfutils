f88c51c56e305e08406bc9f21ab0963079f15e30
Erin Kado-Fong <erin.kado-fong@yale.edu>
2024-06-03 10:25:21 -0400

    add ellipsed segmap builder

--- ekfstats/ekfstats/imstats.py
+++ ekfstats/ekfstats/imstats.py
@@ -62,3 +62,50 @@ def build_xygrid ( shape, x_0=None, y_0=None, theta=0., ellip=0.):
     Y_rot /=  1. - ellip
     R = np.sqrt(X_rot**2 + Y_rot**2) + 0.0001    
     return R
+
+def build_ellipse_from_sep ( row, cutout_shape, ellipse_size=9.,  ):
+    """Builds an elliptical region based on SExtractor parameters.
+
+    This function creates an elliptical region using parameters from a given
+    row. The region is defined within a cutout shape.
+
+    Args:
+        row (dict): A dictionary containing SExtractor parameters. Expected keys
+            are 'y', 'x', 'cyy', 'cxx', and 'cxy'.
+        cutout_shape (tuple): A tuple (height, width) representing the shape of
+            the cutout in which the ellipse is created.
+        ellipse_size (float, optional): Size of the ellipse. Default is 9.0.
+
+    Returns:
+        numpy.ndarray: A boolean array where True represents the region inside
+        the ellipse.
+    """    
+    Y,X = np.mgrid[:cutout_shape[1],:cutout_shape[0]]
+        
+    yoff = Y-row['y']
+    xoff = X-row['x']
+    ep = row['cyy']*yoff**2 + row['cxx']*xoff**2 + row['cxy']*xoff*yoff            
+    regionauto = ep < ellipse_size # this value comes from SExtractor manual :shrug: 
+    return regionauto
+
+def build_ellipsed_segmentationmap ( sep_catalog, cutout_shape ):
+    """Builds a segmentation map with elliptical regions.
+
+    This function constructs a segmentation map by adding elliptical regions
+    for each entry in the provided SExtractor catalog.
+
+    Args:
+        sep_catalog (list of dict): A list of dictionaries, each containing
+            SExtractor parameters for a different object. Each dictionary should
+            have keys 'y', 'x', 'cyy', 'cxx', and 'cxy'.
+        cutout_shape (tuple): A tuple (height, width) representing the shape of
+            the cutout in which the ellipses are created.
+
+    Returns:
+        numpy.ndarray: An integer array representing the segmentation map, where
+        each ellipse is added to the map.
+    """    
+    segmap = np.zeros(cutout_shape, dtype=int)
+    for cid in range(len(sep_catalog)):
+        segmap += build_ellipse_from_sep ( sep_catalog[cid], cutout_shape, )
+    return segmap
