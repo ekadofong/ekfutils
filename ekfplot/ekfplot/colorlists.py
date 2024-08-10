6d5020c76b06194f2c96da9a5d258965429efe6c
Erin Kado-Fong <erin.kado-fong@yale.edu>
2024-08-10 08:37:38 -0400

    start adding predetermined color mapping

---
+++ ekfplot/ekfplot/colorlists.py
@@ -0,0 +1,22 @@
+# https://eleanormaclure.wordpress.com/wp-content/uploads/2011/03/colour-coding.pdf
+london_rail_colors = {
+    "one": '#5A6A91',
+    #"gatwick_express": '#151515', # \\ basically black
+    #"gt_n._eastern": '#D89EB6', # \\ too pale
+    "chiltern": '#B7AECF',
+    "central": '#82C8E6',
+    "wessex": '#96B17F',
+    "south_eastern": '#ECC15F',
+    "silverlink": '#E39F54',
+    "south_west": '#D5492D',
+    "first_gt_western": '#6A0B1E',
+    "c2c": '#CF609F',
+    "island_line": '#AC4D9F',
+    "thameslink": '#943F98',
+    "heathrow_express": '#9FCAA9',
+    "first_gt_western_link": '#B9B86A',
+    "southern": '#6A9B5F',
+    "virgin": '#EEAF7F',
+    "wagn": '#A66D44',
+    "midland_mainline": '#2F4C8F'
+}
