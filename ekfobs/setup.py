3bc2f2edd4b444c50955edbf3f2d15a4b16a3a10
Erin Kado-Fong <erin.kado-fong@yale.edu>
2024-08-29 11:37:46 -0400

    add ekfobs

---
+++ ekfobs/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup (
+    name = 'ekfobs',
+    version='0.0.1',
+    author='Erin Kado-Fong',
+    description='handy dandy observation utils',
+    packages=setuptools.find_packages(),
+    #python_requires='>=3.7'
+    )
