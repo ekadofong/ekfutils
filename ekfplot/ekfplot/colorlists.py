923662a6f69e943a8d3428c9018496179871abf6
Erin Kado-Fong <erin.kado-fong@yale.edu>
2025-01-27 14:15:53 -0500

    add colors

--- ekfplot/ekfplot/colorlists.py
+++ ekfplot/ekfplot/colorlists.py
@@ -20,3 +20,26 @@ london_rail_colors = {
     "wagn": '#A66D44',
     "midland_mainline": '#2F4C8F'
 }
+
+hcbold = {
+    'red':'#e66a4e',
+	'orange':'#eb7f2c',
+	'green':'#6fb087',
+	'blue':'#65a7c5',
+	'grey':'#c6caca',    
+}
+
+coolwarm2025 = {
+    'teal0':'#69d2e6',
+	'teal1':'#a7dbd8',
+	'neutral':'#e0e4cc',
+	'orange1':'#f38630',
+	'orange0':'#fa690',
+}
+
+slides = {
+    'red':'#E06950', 
+    'orange':'#E59858', 
+    'yellow':'#E9C46A', 
+    'blue':'#4BB8CD'
+}
