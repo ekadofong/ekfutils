db46b260983c553bee662b62a55ccc1d8fb17fc6
Erin Kado-Fong <erin.kado-fong@yale.edu>
2025-07-09 10:43:48 -0400

    faster way to generate multivariate gaussians

--- ekfstats/ekfstats/functions.py
+++ ekfstats/ekfstats/functions.py
@@ -1,4 +1,6 @@
 import numpy as np
+from numba import njit, prange
+from scipy import linalg
 from scipy.special import erf
 
 def wide_kdeBW ( size, alpha=3. ):
@@ -122,3 +124,137 @@ def cauchy_pdf(x, x0, gamma):
     """
     return 1 / (np.pi * gamma * (1 + ((x - x0) / gamma)**2))
 
+
+def log_multivariate_gaussian(x, mu, V, Vinv=None, method=1):
+    """
+    EXTRACTION OF astroML.utils.log_multivariate_gaussian
+    by https://github.com/astroML/astroML
+    
+    Evaluate a multivariate gaussian N(x|mu, V)
+
+    This allows for multiple evaluations at once, using array broadcasting
+
+    Parameters
+    ----------
+    x: array_like
+        points, shape[-1] = n_features
+
+    mu: array_like
+        centers, shape[-1] = n_features
+
+    V: array_like
+        covariances, shape[-2:] = (n_features, n_features)
+
+    Vinv: array_like or None
+        pre-computed inverses of V: should have the same shape as V
+
+    method: integer, optional
+        method = 0: use cholesky decompositions of V
+        method = 1: use explicit inverse of V
+
+    Returns
+    -------
+    values: ndarray
+        shape = broadcast(x.shape[:-1], mu.shape[:-1], V.shape[:-2])
+
+    Examples
+    --------
+
+    >>> x = [1, 2]
+    >>> mu = [0, 0]
+    >>> V = [[2, 1], [1, 2]]
+    >>> log_multivariate_gaussian(x, mu, V)
+    -3.3871832107434003
+    """
+    x = np.asarray(x, dtype=float)
+    mu = np.asarray(mu, dtype=float)
+    V = np.asarray(V, dtype=float)
+
+    ndim = x.shape[-1]
+    x_mu = x - mu
+
+    if V.shape[-2:] != (ndim, ndim):
+        raise ValueError("Shape of (x-mu) and V do not match")
+
+    Vshape = V.shape
+    V = V.reshape([-1, ndim, ndim])
+
+    if Vinv is not None:
+        assert Vinv.shape == Vshape
+        method = 1
+
+    if method == 0:
+        Vchol = np.array([linalg.cholesky(V[i], lower=True)
+                          for i in range(V.shape[0])])
+
+        # we may be more efficient by using scipy.linalg.solve_triangular
+        # with each cholesky decomposition
+        VcholI = np.array([linalg.inv(Vchol[i])
+                          for i in range(V.shape[0])])
+        logdet = np.array([2 * np.sum(np.log(np.diagonal(Vchol[i])))
+                           for i in range(V.shape[0])])
+
+        VcholI = VcholI.reshape(Vshape)
+        logdet = logdet.reshape(Vshape[:-2])
+
+        VcIx = np.sum(VcholI * x_mu.reshape(x_mu.shape[:-1]
+                                            + (1,) + x_mu.shape[-1:]), -1)
+        xVIx = np.sum(VcIx ** 2, -1)
+
+    elif method == 1:
+        if Vinv is None:
+            Vinv = np.array([linalg.inv(V[i])
+                             for i in range(V.shape[0])]).reshape(Vshape)
+        else:
+            assert Vinv.shape == Vshape
+
+        logdet = np.log(np.array([linalg.det(V[i])
+                                  for i in range(V.shape[0])]))
+        logdet = logdet.reshape(Vshape[:-2])
+
+        xVI = np.sum(x_mu.reshape(x_mu.shape + (1,)) * Vinv, -2)
+        xVIx = np.sum(xVI * x_mu, -1)
+
+    else:
+        raise ValueError("unrecognized method %s" % method)
+
+    return -0.5 * ndim * np.log(2 * np.pi) - 0.5 * (logdet + xVIx)
+
+@njit(parallel=True)
+def fast_log_multivariate_gaussian(x, mu, V, Vinv=None, method=1):
+    x = np.asarray(x, dtype=np.float64)
+    mu = np.asarray(mu, dtype=np.float64)
+    V = np.asarray(V, dtype=np.float64)
+
+    ndim = x.shape[-1]
+    x_mu = x - mu
+
+    if V.shape[-2:] != (ndim, ndim):
+        raise ValueError("Shape of (x-mu) and V do not match")
+
+    Vshape = V.shape
+    V_reshaped = V.reshape(-1, ndim, ndim)
+    n_matrices = V_reshaped.shape[0]
+
+    logdet = np.empty(n_matrices, dtype=np.float64)
+    xVIx = np.empty(n_matrices, dtype=np.float64)
+
+    if method == 1:
+        if Vinv is None:
+            Vinv = np.empty_like(V_reshaped)
+            for i in prange(n_matrices):
+                Vinv[i] = np.linalg.inv(V_reshaped[i])
+        else:
+            Vinv = Vinv.reshape(-1, ndim, ndim)
+
+        for i in prange(n_matrices):
+            logdet[i] = np.log(np.linalg.det(V_reshaped[i]))
+            x_mu_i = x_mu[i]
+            xVI = np.dot(Vinv[i], x_mu_i)
+            xVIx[i] = np.dot(x_mu_i, xVI)
+
+    else:
+        raise ValueError("Only method=1 is supported in fast version.")
+
+    const = -0.5 * ndim * np.log(2 * np.pi)
+    return const - 0.5 * (logdet + xVIx)
